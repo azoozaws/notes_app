dc1d190668f7f2f640e26d94966bdbc9e63845dd
azoozaws <azoozaws24@gmail.com>
2024-01-28 04:17:09 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() {
-  runApp( NotesApp());
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
