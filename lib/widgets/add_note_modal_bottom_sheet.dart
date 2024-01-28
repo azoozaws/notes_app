dd800fa6b04c8841e011121a7254af384722eb46
azoozaws <azoozaws24@gmail.com>
2024-01-28 16:14:08 +0300

    not important just remove import

--- lib/widgets/add_note_modal_bottom_sheet.dart
+++ lib/widgets/add_note_modal_bottom_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:notes_app/widgets/custom_Text_Field.dart';
 
 class AddNoteModalBottomSheet extends StatelessWidget {
